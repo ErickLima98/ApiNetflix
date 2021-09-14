54fdb26d5ba326e88e3fcf0a19ce6327fbd85dca
Alle <allecan52@gmail.com>
2021-09-13 19:57:28 -0600

    Asignando variables para las tags escenciales

--- getDataYouTube.py
+++ getDataYouTube.py
@@ -1,17 +1,22 @@
 from googleapiclient.discovery import build
-api_key = 'AIzaSyCytYniTsTOWwhMZPJSj27bH7WdAD9tJBU'
-youtube = build('youtube', 'v3', developerKey = api_key)
+#import pandas as pd
 
-request = youtube.channels().list(
-    part='statistics',
-    id = 'UCxqDIXlqSU1ksBuIJNk3Aog'
-)
-request2 = youtube.channels().list(
-    part='statistics',
-    forUsername = 'francoescamilla'
-      
-)
-response = request.execute()
-response2 = request2.execute()
-print(response)
-print(response2)
+api_key = 'AIzaSyBKe0n9tKUtFh7Bja5DUXab3mT-ZDUe4Ww'
+youtube = build('youtube', 'v3', developerKey=api_key)
+snippets = youtube.search().list(part='snippet', type='channel', q='Zellendust').execute()
+# for i in snippets['items']:
+#     print(i)
+channelId = snippets['items'][0]['snippet']['channelId']
+print(f'ID del 1er canal: {channelId}')
+print('Descripcion del canal: ')
+print(snippets['items'][0]['snippet']['description'] + '\n')
+
+stats = youtube.channels().list(part='statistics', id=channelId).execute()
+subscribers = stats['items'][0]['statistics']['subscriberCount']
+contVistas = stats['items'][0]['statistics']['viewCount']
+cantVideos = stats['items'][0]['statistics']['videoCount']
+print(f'Subscritores: {int(subscribers).__format__(",")}')
+print(f'Vistas del canal: {int(contVistas).__format__(",")}')
+print(f'Total de videos: {int(cantVideos).__format__(",")}')
+# for i in stats['items']:
+#     print(i)
