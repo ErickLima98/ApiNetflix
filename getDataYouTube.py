0b6c3ec1782d86cd647a9b9dea40a78bfdbf9c5c
Alle <allecan52@gmail.com>
2021-09-14 18:19:49 -0600

    algoritmo para inserte a MongoDB

--- getDataYouTube.py
+++ getDataYouTube.py
@@ -1,12 +1,18 @@
 from googleapiclient.discovery import build
-from googleapiclient.discovery_cache.file_cache import FILENAME
+#from googleapiclient.discovery_cache.file_cache import FILENAME
+import pymongo
 #import pandas as pd
 
-#Canal 1
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["pruebaAPI"]
+mycol = mydb["Youtube"]
 
 api_key = 'AIzaSyBKe0n9tKUtFh7Bja5DUXab3mT-ZDUe4Ww'
 youtube = build('youtube', 'v3', developerKey=api_key)
-snippets = youtube.search().list(part='snippet', type='channel', q='TheGrefg').execute()
+
+#Canal 1
+channelName = 'TheGrefg'
+snippets = youtube.search().list(part='snippet', type='channel', q=channelName).execute()
 
 channelId = snippets['items'][0]['snippet']['channelId']
 stats = youtube.channels().list(part='statistics', id=channelId).execute()
@@ -21,13 +27,15 @@ print(f'ID del 1er canal: {channelId}')
 print('Descripcion del canal: ')
 print(snippets['items'][0]['snippet']['description'] + '\n')
 
+mydict = {"ChannelName": channelName, "Subscribers": subscribers,
+          "ViewsCount": contVistas, "VideosCount": cantVideos}
+x = mycol.insert_one(mydict)
+
 #-----------------------------------------------------------------------------------------------------------
 
 #Canal 2
-
-api_key = 'AIzaSyBKe0n9tKUtFh7Bja5DUXab3mT-ZDUe4Ww'
-youtube = build('youtube', 'v3', developerKey=api_key)
-snippets = youtube.search().list(part='snippet', type='channel', q='Luisito Comunica').execute()
+channelName = 'Luisito Comunica'
+snippets = youtube.search().list(part='snippet', type='channel', q=channelName).execute()
 
 channelId = snippets['items'][0]['snippet']['channelId']
 stats = youtube.channels().list(part='statistics', id=channelId).execute()
@@ -42,20 +50,21 @@ print(f'ID del 2do canal: {channelId}')
 print('Descripcion del canal: ')
 print(snippets['items'][0]['snippet']['description'] + '\n')
 
+mydict = {"ChannelName": channelName, "Subscribers": subscribers,
+          "ViewsCount": contVistas, "VideosCount": cantVideos}
+x = mycol.insert_one(mydict)
 #-----------------------------------------------------------------------------------------------------------
 
 #Canal 3
-
-api_key = 'AIzaSyBKe0n9tKUtFh7Bja5DUXab3mT-ZDUe4Ww'
-youtube = build('youtube', 'v3', developerKey=api_key)
-snippets = youtube.search().list(part='snippet', type='channel', q='Ubaman').execute()
+channelName = 'Ubaman'
+snippets = youtube.search().list(part='snippet', type='channel', q=channelName).execute()
 
 channelId = snippets['items'][0]['snippet']['channelId']
 stats = youtube.channels().list(part='statistics', id=channelId).execute()
 subscribers = stats['items'][0]['statistics']['subscriberCount']
 contVistas = stats['items'][0]['statistics']['viewCount']
 cantVideos = stats['items'][0]['statistics']['videoCount']
-print(f'Nombre del Canal: Uabman')
+print(f'Nombre del Canal: Ubaman')
 print(f'Subscritores: {int(subscribers).__format__(",")}')
 print(f'Vistas del canal: {int(contVistas).__format__(",")}')
 print(f'Total de videos: {int(cantVideos).__format__(",")}')
@@ -63,13 +72,14 @@ print(f'ID del 3er canal: {channelId}')
 print('Descripcion del canal: ')
 print(snippets['items'][0]['snippet']['description'] + '\n')
 
+mydict = {"ChannelName": channelName, "Subscribers": subscribers,
+          "ViewsCount": contVistas, "VideosCount": cantVideos}
+x = mycol.insert_one(mydict)
 #-----------------------------------------------------------------------------------------------------------
 
 #Canal 4
-
-api_key = 'AIzaSyBKe0n9tKUtFh7Bja5DUXab3mT-ZDUe4Ww'
-youtube = build('youtube', 'v3', developerKey=api_key)
-snippets = youtube.search().list(part='snippet', type='channel', q='Coreano Vlogs').execute()
+channelName = 'Coreano Vlogs'
+snippets = youtube.search().list(part='snippet', type='channel', q=channelName).execute()
 
 channelId = snippets['items'][0]['snippet']['channelId']
 stats = youtube.channels().list(part='statistics', id=channelId).execute()
@@ -83,14 +93,15 @@ print(f'Total de videos: {int(cantVideos).__format__(",")}')
 print(f'ID del 4to canal: {channelId}')
 print('Descripcion del canal: ')
 print(snippets['items'][0]['snippet']['description'] + '\n')
+mydict = {"ChannelName": channelName, "Subscribers": subscribers,
+          "ViewsCount": contVistas, "VideosCount": cantVideos}
+x = mycol.insert_one(mydict)
 
 #-----------------------------------------------------------------------------------------------------------
 
 #Canal 5
-
-api_key = 'AIzaSyBKe0n9tKUtFh7Bja5DUXab3mT-ZDUe4Ww'
-youtube = build('youtube', 'v3', developerKey=api_key)
-snippets = youtube.search().list(part='snippet', type='channel', q='Auron').execute()
+channelName='Auron'
+snippets = youtube.search().list(part='snippet', type='channel', q=channelName).execute()
 
 channelId = snippets['items'][0]['snippet']['channelId']
 stats = youtube.channels().list(part='statistics', id=channelId).execute()
@@ -105,16 +116,19 @@ print(f'ID del 5to canal: {channelId}')
 print('Descripcion del canal: ')
 print(snippets['items'][0]['snippet']['description'] + '\n') 
 
+mydict = {"ChannelName": channelName, "Subscribers": subscribers,
+          "ViewsCount": contVistas, "VideosCount": cantVideos}
+x = mycol.insert_one(mydict)
 #-----------------------------------------------------------------------------------------------------------
 #Crea archvio JSON para exportar luego a la base de datos
 
-def creararchivo():
-    archivo=open('datos.json', 'w')
-    archivo.close()
+# def creararchivo():
+#     archivo=open('datos.json', 'w')
+#     archivo.close()
 
-def escribir():
-    archivo=open('datos.json', 'a')
-    archivo.write('hola mundo')
+# def escribir():
+#     archivo=open('datos.json', 'a')
+#     archivo.write('hola mundo')
 
-creararchivo()
-escribir()  
+# creararchivo()
+# escribir()  
