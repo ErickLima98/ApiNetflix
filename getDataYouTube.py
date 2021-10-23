c19cf50add6c1977674c8e1df262a734d3120c32
Alle <allecan52@gmail.com>
2021-10-23 15:15:08 -0600

    arreglando getdata

--- getDataYouTube.py
+++ getDataYouTube.py
@@ -1,175 +1,34 @@
 from googleapiclient.discovery import build
+from datetime import date
 import pymongo
-import mysql.connector
-
-
-def insert_my_sql(chanName, subs, views, videos):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="password", #aqui va la password de sql propia de cada uno
-        database="pruebaapi"
-    )
-    mycursor = mydb.cursor()
-
-    sql = "INSERT INTO youtube (ChannelName, Subscribers, ViewsCount, VideosCount) VALUES (%s, %s, %s, %s)"
-    values = (chanName, subs, views, videos)
-    mycursor.execute(sql, values)
-    mydb.commit()
-
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["pruebaAPI"]
 mycol = mydb["Youtube"]
 
-api_key = 'AIzaSyBKe0n9tKUtFh7Bja5DUXab3mT-ZDUe4Ww'
+#api_key = 'AIzaSyAaKRca21OCUddTBS5SsH0viC1aqoIFtdo'
+api_key = 'AIzaSyDng0e4Oq_NZjGuQLVm9BfKMO4c_AgjBXI'
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 
-# Canal 1
-channelName = 'TheGrefg'
-snippets = youtube.search().list(
-    part='snippet', type='channel', q=channelName).execute()
+def insertChannelMongo(nameChannel):
+    snippets = youtube.search().list(
+        part='snippet', type='channel', q=nameChannel, maxResults=1).execute()
+    channelId = snippets['items'][0]['snippet']['channelId']
+    stats = youtube.channels().list(part='statistics', id=channelId).execute()
+    subscribers = stats['items'][0]['statistics']['subscriberCount']
+    contVistas = stats['items'][0]['statistics']['viewCount']
+    cantVideos = stats['items'][0]['statistics']['videoCount']
+    today = date.today()
+    # dd/mm/YY
+    dateA = today.strftime("%d/%m/%Y")
+    mydict = {"ChannelName": nameChannel, "Subscribers": int(subscribers),
+              "ViewsCount": int(contVistas), "VideosCount": int(cantVideos), "Fecha": dateA}
+    x = mycol.insert_one(mydict)
 
-channelId = snippets['items'][0]['snippet']['channelId']
-stats = youtube.channels().list(part='statistics', id=channelId).execute()
-subscribers = stats['items'][0]['statistics']['subscriberCount']
-contVistas = stats['items'][0]['statistics']['viewCount']
-cantVideos = stats['items'][0]['statistics']['videoCount']
-print(f'Nombre del Canal: TheGrefg')
-print(f'Subscritores: {int(subscribers).__format__(",")}')
-print(f'Vistas del canal: {int(contVistas).__format__(",")}')
-print(f'Total de videos: {int(cantVideos).__format__(",")}')
-print(f'ID del 1er canal: {channelId}')
-print('Descripcion del canal: ')
-print(snippets['items'][0]['snippet']['description'] + '\n')
 
-mydict = {"ChannelName": channelName, "Subscribers": int(subscribers),
-          "ViewsCount": int(contVistas), "VideosCount": int(cantVideos)}
-x = mycol.insert_one(mydict)
-
-insert_my_sql(channelName, int(subscribers), int(contVistas), int(cantVideos))
-# -----------------------------------------------------------------------------------------------------------
-
-# Canal 2
-channelName = 'Luisito Comunica'
-snippets = youtube.search().list(
-    part='snippet', type='channel', q=channelName).execute()
-
-channelId = snippets['items'][0]['snippet']['channelId']
-stats = youtube.channels().list(part='statistics', id=channelId).execute()
-subscribers = stats['items'][0]['statistics']['subscriberCount']
-contVistas = stats['items'][0]['statistics']['viewCount']
-cantVideos = stats['items'][0]['statistics']['videoCount']
-print(f'Nombre del Canal: Luisito Comunica')
-print(f'Subscritores: {int(subscribers).__format__(",")}')
-print(f'Vistas del canal: {int(contVistas).__format__(",")}')
-print(f'Total de videos: {int(cantVideos).__format__(",")}')
-print(f'ID del 2do canal: {channelId}')
-print('Descripcion del canal: ')
-print(snippets['items'][0]['snippet']['description'] + '\n')
-
-mydict = {"ChannelName": channelName, "Subscribers": int(subscribers),
-          "ViewsCount": int(contVistas), "VideosCount": int(cantVideos)}
-x = mycol.insert_one(mydict)
-
-insert_my_sql(channelName, int(subscribers), int(contVistas), int(cantVideos))
-# -----------------------------------------------------------------------------------------------------------
-
-# Canal 3
-channelName = 'Ubaman'
-snippets = youtube.search().list(
-    part='snippet', type='channel', q=channelName).execute()
-
-channelId = snippets['items'][0]['snippet']['channelId']
-stats = youtube.channels().list(part='statistics', id=channelId).execute()
-subscribers = stats['items'][0]['statistics']['subscriberCount']
-contVistas = stats['items'][0]['statistics']['viewCount']
-cantVideos = stats['items'][0]['statistics']['videoCount']
-print(f'Nombre del Canal: Ubaman')
-print(f'Subscritores: {int(subscribers).__format__(",")}')
-print(f'Vistas del canal: {int(contVistas).__format__(",")}')
-print(f'Total de videos: {int(cantVideos).__format__(",")}')
-print(f'ID del 3er canal: {channelId}')
-print('Descripcion del canal: ')
-print(snippets['items'][0]['snippet']['description'] + '\n')
-
-mydict = {"ChannelName": channelName, "Subscribers": int(subscribers),
-          "ViewsCount": int(contVistas), "VideosCount": int(cantVideos)}
-x = mycol.insert_one(mydict)
-
-insert_my_sql(channelName, int(subscribers), int(contVistas), int(cantVideos))
-# -----------------------------------------------------------------------------------------------------------
-
-# Canal 4
-channelName = 'Coreano Vlogs'
-snippets = youtube.search().list(
-    part='snippet', type='channel', q=channelName).execute()
-
-channelId = snippets['items'][0]['snippet']['channelId']
-stats = youtube.channels().list(part='statistics', id=channelId).execute()
-subscribers = stats['items'][0]['statistics']['subscriberCount']
-contVistas = stats['items'][0]['statistics']['viewCount']
-cantVideos = stats['items'][0]['statistics']['videoCount']
-print(f'Nombre del Canal: Coreano Vlogs')
-print(f'Subscritores: {int(subscribers).__format__(",")}')
-print(f'Vistas del canal: {int(contVistas).__format__(",")}')
-print(f'Total de videos: {int(cantVideos).__format__(",")}')
-print(f'ID del 4to canal: {channelId}')
-print('Descripcion del canal: ')
-print(snippets['items'][0]['snippet']['description'] + '\n')
-mydict = {"ChannelName": channelName, "Subscribers": int(subscribers),
-          "ViewsCount": int(contVistas), "VideosCount": int(cantVideos)}
-x = mycol.insert_one(mydict)
-
-insert_my_sql(channelName, int(subscribers), int(contVistas), int(cantVideos))
-# -----------------------------------------------------------------------------------------------------------
-
-# Canal 5
-channelName = 'Auron'
-snippets = youtube.search().list(
-    part='snippet', type='channel', q=channelName).execute()
-
-channelId = snippets['items'][0]['snippet']['channelId']
-stats = youtube.channels().list(part='statistics', id=channelId).execute()
-subscribers = stats['items'][0]['statistics']['subscriberCount']
-contVistas = stats['items'][0]['statistics']['viewCount']
-cantVideos = stats['items'][0]['statistics']['videoCount']
-print(f'Nombre del Canal: Auron')
-print(f'Subscritores: {int(subscribers).__format__(",")}')
-print(f'Vistas del canal: {int(contVistas).__format__(",")}')
-print(f'Total de videos: {int(cantVideos).__format__(",")}')
-print(f'ID del 5to canal: {channelId}')
-print('Descripcion del canal: ')
-print(snippets['items'][0]['snippet']['description'] + '\n')
-
-mydict = {"ChannelName": channelName, "Subscribers": int(subscribers),
-          "ViewsCount": int(contVistas), "VideosCount": int(cantVideos)}
-x = mycol.insert_one(mydict)
-
-insert_my_sql(channelName, int(subscribers), int(contVistas), int(cantVideos))
-# -----------------------------------------------------------------------------------------------------------
-
-# Canal 6
-channelName = 'Franco Escamilla'
-snippets = youtube.search().list(
-    part='snippet', type='channel', q=channelName).execute()
-
-channelId = snippets['items'][0]['snippet']['channelId']
-stats = youtube.channels().list(part='statistics', id=channelId).execute()
-subscribers = stats['items'][0]['statistics']['subscriberCount']
-contVistas = stats['items'][0]['statistics']['viewCount']
-cantVideos = stats['items'][0]['statistics']['videoCount']
-print(f'Nombre del Canal: TheGrefg')
-print(f'Subscritores: {int(subscribers).__format__(",")}')
-print(f'Vistas del canal: {int(contVistas).__format__(",")}')
-print(f'Total de videos: {int(cantVideos).__format__(",")}')
-print(f'ID del 1er canal: {channelId}')
-print('Descripcion del canal: ')
-print(snippets['items'][0]['snippet']['description'] + '\n')
-
-mydict = {"ChannelName": channelName, "Subscribers": int(subscribers),
-          "ViewsCount": int(contVistas), "VideosCount": int(cantVideos)}
-x = mycol.insert_one(mydict)
-
-insert_my_sql(channelName, int(subscribers), int(contVistas), int(cantVideos))
+lista_canales = ["Franco Escamilla", "TheGrefg", "Luisito Comunica",
+                 "Ubaman", "Coreano Vlogs", "Auron", ]
+for canal in lista_canales:
+    insertChannelMongo(canal)
+    
